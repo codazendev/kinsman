b9201a68b5af6bd17b0faf2ed436eb80fb85f592
Jeremy King <codazendev@gmail.com>
2025-10-28 17:13:40 -0400

    added SDL window creation to main file

--- src/kinsman.cpp
+++ src/kinsman.cpp
@@ -6,6 +6,11 @@ int main(int argc, char *argv[]) {
     // TODO: SDL initialization failed
   }
 
+  // Create window variable
+  SDL_Window *Window;
+
+  Window = SDL_CreateWindow("Kinsman", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_RESIZABLE);
+
   // Tear down SDL
   SDL_Quit();
 
