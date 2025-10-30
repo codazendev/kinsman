173ee7c8afce5c46ccc43dd84263160424d261dd
Jeremy King <codazendev@gmail.com>
2025-10-29 20:19:45 -0400

    added compile_commands.json generation to compilation script

--- src/build.sh
+++ src/build.sh
@@ -2,5 +2,5 @@
 
 mkdir -p ../build
 pushd ../build
-c++ `sdl2-config --cflags --libs` ../src/kinsman.cpp -o kinsman -g
+bear -- c++ `sdl2-config --cflags --libs` ../src/kinsman.cpp -o kinsman -g
 popd
